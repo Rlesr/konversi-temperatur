0ea811b5a331da8ded93cb251a8b62c7b8cb01f8
Rlesr <122105014+Rlesr@users.noreply.github.com>
2023-03-28 08:38:10 +0700

    Add files via upload

--- ctof.py
+++ ctof.py
@@ -1,4 +1,4 @@
 celcius = float(input("Masukan suhu dalam derajat Celcius: "))
 fahrenheit = (9/5 * celcius) + 32
 
-print(celcius, "derajat celcius: ", fahrenheit, "F")
+print(celcius, "derajat celcius sama dengan", fahrenheit, "F")
